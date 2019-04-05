85168ad2ed1e983d8d87b3636a5c486358abf1df
Cory Driscoll <cory@drisc.io>
2019-04-05 15:42:19 +0100

    Moved registration into seperate file, updated code style

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,24 @@
+mod register;
+
 extern crate mammut;
 extern crate rustyline;
-extern crate ncurses as nc;
 extern crate toml;
 
-use std::fs::File;
-use std::io;
-use std::io::prelude::*;
+use std::error;
+use mammut::status_builder::StatusBuilder;
 
-use rustyline::error::ReadlineError;
-use rustyline::Editor;
+use rustyline::{error::ReadlineError,
+                Editor};
 
-use mammut::apps::{AppBuilder, Scopes};
-use mammut::{Data, Mastodon, Registration, StatusBuilder};
 
-const MASTODON_DATA: &str = "mastodon-data.toml";
-
-fn main() {
-
-    init_ncurses();
-
+fn main()  {
     new_status();
-    //let timeline = mastodon.get_home_timeline();
-    //println!("{:?}", timeline);
-
-    //ncurses::refresh();
-    //ncurses::getch();
-
-
 }
 
-fn init_ncurses() {
-    // Create ncurses window and clear it
-    nc::initscr();
-    nc::clear();
 
-    nc::noecho();
-    nc::cbreak();
-    nc::keypad(nc::stdscr(), true);
+fn new_status() -> Result<(), Box<error::Error>>{
 
-    let mut max_x = 0;
-    let mut max_y = 0;
-    nc::getmaxyx(nc::stdscr(), &mut max_y, &mut max_x);
-}
-
-fn new_status() {
-
-    let mastodon = match File::open(MASTODON_DATA) {
-        Ok(f) => load_config(f),
-        Err(_) => register(),
-    };
-
+    let mastodon = register::get_mastodon_data()?;
     let mut rl = Editor::<()>::new();
 
     loop {
@@ -58,64 +27,16 @@ fn new_status() {
         match new_status {
             Ok(line) => {
                 let status = StatusBuilder::new(line);
-                //println!("StatusBuilder = {:#?}", status);
                 mastodon.new_status(status).expect("Couldn't post status");
-                print!("Status Posted!");
+                println!("Status Posted!");
             }
             Err(ReadlineError::Interrupted) => {
                 println!("Closing Procul...");
-                break;
             }
             Err(_) => {
                 println!("No input");
-                break;
             }
         }
 
     }
-}
-
-fn register() -> Mastodon {
-    let app = AppBuilder {
-        client_name: "Procul",
-        redirect_uris: "urn:ietf:wg:oauth:2.0:oob",
-        scopes: Scopes::All,
-        website: Some("https://drisc.io/wiki/procul"),
-    };
-
-    print!("Enter instance name: ");
-    io::stdout().flush().unwrap();
-    let mut instance_name = String::new();
-    io::stdin()
-        .read_line(&mut instance_name)
-        .expect("Invalid URL");
-
-    let mut registration = Registration::new(instance_name);
-    registration.register(app).unwrap();;
-    let url = registration.authorise().unwrap();
-
-    print!("Click this link to authorize on Mastodon: {}", url);
-    io::stdout().flush().unwrap();
-    print!("Paste the returned authorization code: ");
-    io::stdout().flush().unwrap();
-
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-
-    let code = input.trim();
-    let mastodon = registration.create_access_token(code.to_string()).unwrap();
-
-    // Save app data for using on the next run.
-    let toml = toml::to_string(&*mastodon).unwrap();
-    let mut file = File::create(MASTODON_DATA).unwrap();
-    file.write_all(toml.as_bytes()).unwrap();
-
-    mastodon
-}
-
-fn load_config(mut file: File) -> Mastodon {
-    let mut config = String::new();
-    file.read_to_string(&mut config).unwrap();
-    let data: Data = toml::from_str(&config).unwrap();
-    Mastodon::from_data(data)
 }
